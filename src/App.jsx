6bbd179eec39876f422f35301fedb297a7825a4b
Muktadul Islam <76801029+MuktadulIslam@users.noreply.github.com>
2023-12-28 01:39:09 +0600

    UPDATE: detect mobile screenshoot

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,85 @@
+// import React, { useEffect } from 'react';
+
+// const DemoDisablePrintScreen = () => {
+//   useEffect(() => {
+//     const handleKeyUp = (e) => {
+//       if (e.key === 'PrintScreen') {
+//         navigator.clipboard.writeText('');
+//         alert('Screenshots disabled!');
+//       }
+//     };
+
+//     document.addEventListener('keyup', handleKeyUp);
+
+//     return () => {
+//       document.removeEventListener('keyup', handleKeyUp);
+//     };
+//   }, []);
+
+//   return (
+//     <div id="main">
+//       <h2>Codebrary</h2>
+//       <p>
+//         Click anywhere on the green background and Press key <b>PrtScr</b> or <b>Alt+PrntScr</b> and try to paste the
+//         content in your favorite Picture Editor.
+//       </p>
+//       <a href="https://www.codebrary.com" target="_blank" rel="noopener noreferrer">
+//         www.codebrary.com
+
+//       </a>
+//     </div>
+//   );
+// };
+
+// export default DemoDisablePrintScreen;
+
+
 import React, { useEffect } from 'react';
 
-const DemoDisablePrintScreen = () => {
+const ScreenshotDetection = () => {
   useEffect(() => {
-    const handleKeyUp = (e) => {
-      if (e.key === 'PrintScreen') {
-        navigator.clipboard.writeText('');
-        alert('Screenshots disabled!');
+    // Clipboard API detection
+    const checkClipboard = async () => {
+      try {
+        const text = await navigator.clipboard.readText();
+        if (text.startsWith('data:image/')) {
+          alert('Screenshot might have been taken!');
+        }
+      } catch (error) {
+        console.error('Clipboard API error:', error);
       }
     };
 
-    document.addEventListener('keyup', handleKeyUp);
+    // Canvas manipulation detection
+    const canvas = document.getElementById('myCanvas');
+    const initialDataURL = canvas.toDataURL();
 
-    return () => {
-      document.removeEventListener('keyup', handleKeyUp);
+    const checkCanvasChange = () => {
+      const currentDataURL = canvas.toDataURL();
+      if (initialDataURL !== currentDataURL) {
+        alert('Screenshot might have been taken!');
+      }
     };
-  }, []);
+
+    // Interval for canvas check
+    const canvasInterval = setInterval(checkCanvasChange, 500);
+
+    // Cleanup on component unmount
+    return () => {
+      clearInterval(canvasInterval);
+    };
+
+    // Check clipboard on component mount
+    checkClipboard();
+  }, []); // Empty dependency array to run effect only once on mount
 
   return (
-    <div id="main">
-      <h2>Codebrary</h2>
-      <p>
-        Click anywhere on the green background and Press key <b>PrtScr</b> or <b>Alt+PrntScr</b> and try to paste the
-        content in your favorite Picture Editor.
-      </p>
-      <a href="https://www.codebrary.com" target="_blank" rel="noopener noreferrer">
-        www.codebrary.com
-        
-      </a>
+    <div>
+      <h1>Your React App Content Goes Here</h1>
+      <canvas id="myCanvas" width="400" height="200"></canvas>
     </div>
   );
 };
 
-export default DemoDisablePrintScreen;
+export default ScreenshotDetection;
+
